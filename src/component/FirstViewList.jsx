89cc236793ebceaf119b9e641658ecf4cd9f41da
Govind Kumawat <kumawatgovind1997@gmail.com>
2024-09-17 09:23:47 +0530

    newslist

--- src/component/FirstViewList.jsx
+++ src/component/FirstViewList.jsx
@@ -2,19 +2,99 @@ import React from 'react'
 
 const FirstViewList = () => {
   return (
-    <div className='w-3/6'>
-        <div className='flex'>
+    <div>
+     <div className=''>
+        <div className='flex items-center'>
             <div className='pl-6'>
-            <div className='font-semibold'>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
-            <div className='text-sm'>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='font-medium'>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='text-sm font-light pr-4'>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='flex gap-3 text-sm pt-2'>
+            <div className='text-orange-500 font-semibold '>
+                Olympics
+            </div>
+            <div>|</div>
+            <div>6 Minutes Read</div>
+        </div>
             </div>
 
             <div className='h-50 w-50'>
-                <img className='w-100 h-100' src='https://www.hollywoodreporter.com/wp-content/uploads/2023/12/snoopdogg.jpg'/>
+                <img className='w-72 h-34 rounded-md' src='https://www.hollywoodreporter.com/wp-content/uploads/2023/12/snoopdogg.jpg'/>
             </div>
 
         </div>
+       
     </div>
+
+    <div className='pt-5'>
+        <div className='flex items-center'>
+            <div className='pl-6'>
+            <div className='font-medium '>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='text-sm font-light '>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='flex gap-3 text-sm pt-2'>
+            <div className='text-orange-500 font-semibold '>
+                Olympics
+            </div>
+            <div>|</div>
+            <div>6 Minutes Read</div>
+        </div>
+            </div>
+
+            <div className='h-50 w-50'>
+                <img className='w-72 h-34 rounded-md' src='https://www.hollywoodreporter.com/wp-content/uploads/2023/12/snoopdogg.jpg'/>
+            </div>
+
+        </div>
+       
+    </div>
+   
+    <div className='pt-5'>
+        <div className='flex items-center'>
+            <div className='pl-6'>
+            <div className='font-medium'>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='text-sm font-light pr-4'>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='flex gap-3 text-sm pt-2'>
+            <div className='text-orange-500 font-semibold '>
+                Olympics
+            </div>
+            <div>|</div>
+            <div>6 Minutes Read</div>
+        </div>
+            </div>
+
+            <div className='h-50 w-50'>
+                <img className='w-72 h-34 rounded-md' src='https://www.hollywoodreporter.com/wp-content/uploads/2023/12/snoopdogg.jpg'/>
+            </div>
+
+        </div>
+       
+    </div>
+
+    <div className='pt-5'>
+        <div className='flex items-center'>
+            <div className='pl-6'>
+            <div className='font-medium'>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='text-sm font-light pr-4'>Boom, Snoop Dogg: Breaking Electrifies Paris 2024 Olympics</div>
+            <div className='flex gap-3 text-sm pt-2'>
+            <div className='text-orange-500 font-semibold '>
+                Olympics
+            </div>
+            <div>|</div>
+            <div>6 Minutes Read</div>
+        </div>
+            </div>
+
+            <div className='h-50 w-50'>
+                <img className='w-72 h-34 rounded-md' src='https://www.hollywoodreporter.com/wp-content/uploads/2023/12/snoopdogg.jpg'/>
+            </div>
+
+        </div>
+       
+    </div>
+
+    </div>
+   
+
+    
   )
 }
 
